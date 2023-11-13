dc3ad20310361379a5245bbfb0c04246226237fa
ryota <ryota6114@gmail.com>
2023-11-13 10:40:56 +0900

    おみくじ追加

--- bolt.js
+++ bolt.js
@@ -1,17 +1,23 @@
 'use strict';
 const bolt = require('@slack/bolt');
-const dotenv = require ('dotenv');
+const dotenv = require('dotenv');
 dotenv.config();
 
 const app = new bolt.App({
   token: process.env.SLACK_BOT_TOKEN,
   appToken: process.env.SLACK_APP_TOKEN,
   socketMode: true,
-  logLevel: 'debug'
+  logLevel: 'debug',
 });
 
-app.message(/hello/i, ({message, say}) => {
-  say('こんにちは！');
+app.message(/hello/i, ({ message, say }) => {
+  say(`こんにちは！<@${message.user}>さん！`);
+});
+
+app.message('おみくじ', ({ message, say }) => {
+  const lots = ['大吉', '吉', '中吉', '末吉', '凶'];
+  const lot = lots[Math.floor(Math.random() * lots.length)];
+  say(`${lot}, <@${message.user}>`);
 });
 
 app.start();
